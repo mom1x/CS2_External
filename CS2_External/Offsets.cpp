d3cf0e96d579508765ad45245b524ae63dd278fe
Mom1x <momixpro8@gmail.com>
2026-05-19 09:03:19 +0700

    Update Offsets.cpp

--- CS2_External/Offsets.cpp
+++ CS2_External/Offsets.cpp
@@ -1,6 +1,6 @@
 #include "Offsets.h"
 
-// Функцию поиска оставляем пустой, так как сигнатуры больше не используются
+// Пустая функция-заглушка, чтобы проект не ругался
 DWORD64 SearchOffsets(std::string Signature, DWORD64 ModuleAddress)
 {
     return 0;
@@ -12,12 +12,11 @@ bool Offset::UpdateOffsets()
     if (ClientDLL == 0)
         return false;
 
-    // Напрямую записываем актуальные адреса из твоего дампера a2x:
+    // Прописываем только те переменные, которые 100% созданы автором в оригинальном коде:
     Offset::EntityList = 38847920; // dwEntityList
     Offset::LocalPlayerController = 36986192; // dwLocalPlayerController
     Offset::Matrix = 37143280; // dwViewMatrix
     Offset::GlobalVars = 34101128; // dwGlobalVars
-    Offset::ViewAngle = 37207096; // dwViewAngles
     Offset::LocalPlayerPawn = 34146432; // dwLocalPlayerPawn
     Offset::ForceJump = 34119712; // jump
 
